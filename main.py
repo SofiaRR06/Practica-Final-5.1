6c08cecdd228def47d41655414cd81224753c650
SofiaRR06 <alumno.708095@ies-azarquiel.es>
2026-01-27 08:44:52 +0100

    solucionar problema de CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.database import Base, engine
 from routes import platforms, videogames
@@ -6,6 +7,17 @@ from routes import platforms, videogames
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=[
+"http://localhost:5173", # React (Vite)
+"http://localhost:3000" # React (Create React App)
+],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
 
 app.include_router(videogames.router)
 app.include_router(platforms.router)
